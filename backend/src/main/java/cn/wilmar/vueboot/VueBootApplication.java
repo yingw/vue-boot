d0afb4a5c47039cd680005655a9ce66405977ca6
Yin Guo Wei <yinguowei@gmail.com>
2019-04-25 18:26:35 +0800

    format

--- backend/src/main/java/cn/wilmar/vueboot/VueBootApplication.java
+++ backend/src/main/java/cn/wilmar/vueboot/VueBootApplication.java
@@ -14,21 +14,46 @@ public class VueBootApplication {
         SpringApplication.run(VueBootApplication.class, args);
     }
 
-        @RequestMapping("/login")
-        @ResponseBody
-        public String login() {
-            return "OK";
-        }
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login() {
+        return "OK";
+    }
 
-        @RequestMapping("/save_error_logger")
-        @ResponseBody
-        public String saveErrorLogger() {
-            return "Saved!";
-        }
+    @RequestMapping("/save_error_logger")
+    @ResponseBody
+    public String saveErrorLogger() {
+        return "Saved!";
+    }
 
-        @RequestMapping("/get_info")
-        @ResponseBody
-        public String getInfo() {
-            return "info";
-        }
+    @RequestMapping("/get_info")
+    @ResponseBody
+    public String getInfo() {
+        return "info";
+    }
+
+        /*
+            const data = res.data
+            commit('setAvator', data.avator)
+            commit('setUserName', data.name)
+            commit('setUserId', data.user_id)
+            commit('setAccess', data.access)*/
+
+        /*
+  super_admin: {
+    name: 'super_admin',
+    user_id: '1',
+    access: ['super_admin', 'admin'],
+    token: 'super_admin',
+    avator: 'https://file.iviewui.com/dist/a0e88e83800f138b94d2414621bd9704.png'
+  },
+        */
+
+    static class User {
+        String name;
+        String user_id;
+        String[] access;
+        String token;
+        String avator;
+    }
 }
